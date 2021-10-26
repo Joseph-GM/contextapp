505438ae2828545c7e7d2d48ea44f5b004e936f9
Joseph-GM <jkim.english75@gmail.com>
2021-10-26 14:33:48 +0900

    Tutorial #15 updated, adding BookContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,18 +1,23 @@
 import React, {useContext} from 'react'
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext'
 
 function BookList() {
     const context = useContext(ThemeContext);
-    console.log(context);
+    const {books} = useContext(BookContext);
+
+    console.log('in BookList : ', books);
     const { isLightTheme, light, dark} = context.themeState;
     console.log('isLightTheme', isLightTheme);
     const theme = isLightTheme ? light : dark;
     return (
         <div className='book-list' style={{background: theme.bg, color: theme.syntax}}>
             <ul>
-                <li style={{ background: theme.ui}}>the way of kings</li>
-                <li style={{ background: theme.ui}}>the name of the wind</li>
-                <li style={{ background: theme.ui}}>the final empire</li>
+                {books.map(book => {
+                    return (
+                        <li key={book.id} style={{ background: theme.ui}}>{book.title}</li>
+                    );
+                })}
             </ul>
             
         </div>
